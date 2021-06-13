d0fd89539b06d206715cf3376069251affdf118d
kanik155 <takekanik5@gmail.com>
2021-06-13 17:45:07 +0900

    list todo

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -1,6 +1,6 @@
 import { configure } from "@/my-aws-exports"
 import { GraphQLResult } from "@aws-amplify/api-graphql"
-import { API, graphQLOperation } from "aws-amplify"
+import { API, graphqlOperation } from "aws-amplify"
 import Observable from "zen-observable-ts"
 
 configure()
@@ -8,13 +8,13 @@ configure()
 export const query = async <T, V = undefined>
 (query: string, variables?: V) => {
     return (await API.graphql(
-        graphQLOperation(query, variables)
+        graphqlOperation(query, variables)
     )) as GraphQLResult<T>
 }
 
 export const mutate = async <T, V = undefined>(mutation: string, variables?: V) => {
     return (await API.graphql(
-        graphQLOperation(mutation, variables)
+        graphqlOperation(mutation, variables)
     )) as GraphQLResult<T>
 }
 
@@ -22,7 +22,7 @@ export const subscription = <T extends object, V = undefined>(
     subscription: string,
     variables?: V
 ) => {
-    return API.graphql(graphQLOperation(subscription, variables)) as Observable<{
+    return API.graphql(graphqlOperation(subscription, variables)) as Observable<{
         value: {data: T}
     }>
 }
